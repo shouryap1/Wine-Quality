ec38b7e61ee299949e720d7d06fdee1215cb11a2
shouryap1 <shourya.prasad1@gmail.com>
2024-03-24 12:15:07 +0530

    Add Model Evaluation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.wine_quality.pipeline.stage_01_data_ingestion import DataIngestionTrain
 from src.wine_quality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.wine_quality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.wine_quality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-
+from src.wine_quality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -44,3 +44,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+     
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
